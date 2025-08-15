a6e86f234407a3a59ac993608af43b3fc8e6758b
Virtue Engine <101955083+virtuedevelopment@users.noreply.github.com>
2025-08-15 18:29:00 -0400

    Reformatted index.py | 15-08-2025 | Kaine Osiagwu

--- index.py
+++ index.py
@@ -1,21 +1,35 @@
-
+#IMPORTS GO HERE
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot
 
-df=pd.read_csv('Ontario_infaprojects_clean.csv')
 
+#CSV READING DEFINITIONS GO HERE
+print("-----------------------------------------------")
+file_name = input("Welcome to the infastructrue CLI: Enter a database you want to read: " )
+print("-----------------------------------------------")
 
+df=pd.read_csv(file_name)
 df_incomplete = df[df['status'].str.lower() != 'Complete']
 complete_projects = df['status'].str.lower().str.startswith('complete', na=False).sum()
 
 # print("this is whats going on",df_incomplete)
-
 # df_incomplete.to_csv('incomplete_projects.csv', index=False)
+
+
+#CALCULATIONS GO HERE
 total=len(df)
-print ("this is total", total)
 incomplete_projects = total - complete_projects
 Percent_complete = (complete_projects/ total) *100
 Percent_incomplete = (incomplete_projects/ total) *100
-print(Percent_complete)
-print(Percent_incomplete)
+
+#DISPLAYS GO HERE
+print("-----------------------------------------------")
+print("CSV Data information: ")
+print("-----------------------------------------------")
+
+print ("Total Lines: ", total)
+print("Percentage of projects Completed: {:.2f}%".format(Percent_complete))
+print("Percentage of projects Incompleted: {:.2f}%".format(Percent_incomplete))
+
+print("-----------------------------------------------")
